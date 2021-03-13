8feba01b370ad063dfe6991e276e52c3d6546b45
Maksim Rakov <rakov.maksim.2001@mail.ru>
2021-03-13 23:20:19 +0400

    lab_3

---
+++ JewelryShop/JewelryShopDatabaseImplement/Migrations/20210313191841_InitialCreate.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JewelryShopDatabaseImplement.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Components",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComponentName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Components", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jewelrys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JewelryName = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jewelrys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JewelryComponents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JewelryId = table.Column<int>(nullable: false),
+                    ComponentId = table.Column<int>(nullable: false),
+                    Count = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JewelryComponents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JewelryComponents_Components_ComponentId",
+                        column: x => x.ComponentId,
+                        principalTable: "Components",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JewelryComponents_Jewelrys_JewelryId",
+                        column: x => x.JewelryId,
+                        principalTable: "Jewelrys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JewelryId = table.Column<int>(nullable: false),
+                    Count = table.Column<int>(nullable: false),
+                    Sum = table.Column<decimal>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    DateCreate = table.Column<DateTime>(nullable: false),
+                    DateImplement = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Jewelrys_JewelryId",
+                        column: x => x.JewelryId,
+                        principalTable: "Jewelrys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JewelryComponents_ComponentId",
+                table: "JewelryComponents",
+                column: "ComponentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JewelryComponents_JewelryId",
+                table: "JewelryComponents",
+                column: "JewelryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_JewelryId",
+                table: "Orders",
+                column: "JewelryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JewelryComponents");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Components");
+
+            migrationBuilder.DropTable(
+                name: "Jewelrys");
+        }
+    }
+}
